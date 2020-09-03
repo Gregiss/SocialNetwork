dad2fb8aeb07ea9f83d9e0d5c5efdb0d1103e594
Hurricane404 <46573030+Hurricane404@users.noreply.github.com>
2020-09-03 11:24:57 -0300

    Demo review 0.1

---
+++ precache-manifest.57d4ab347d447110c4f1b2380fbb610b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d74e5ddd80d2b9cafead1d83b137154",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be7ef2405d0df8f865e9",
+    "url": "/static/css/2.0a8a8a39.chunk.css"
+  },
+  {
+    "revision": "8e6490817849400b6246",
+    "url": "/static/css/main.7d45d533.chunk.css"
+  },
+  {
+    "revision": "be7ef2405d0df8f865e9",
+    "url": "/static/js/2.68acfa19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.68acfa19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e6490817849400b6246",
+    "url": "/static/js/main.c22e2b1a.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
